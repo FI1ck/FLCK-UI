17c9e89b01a9ed2a1c2b89c55647a4235a4cbd15
Alec <hayalek2000@gmail.com>
2024-04-13 02:39:30 +0400

    added very basic animations using framer motion

--- src/components/Notes_widget.jsx
+++ src/components/Notes_widget.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
 import '../styling/Note.css'
 import { useState } from 'react'
+import { easeInOut, motion, spring } from "framer-motion"
+import { flushSync } from 'react-dom'
+import { easeIn } from 'framer-motion/dom'
 
 const Notes_widget = () => {
 
   const [isOpened, setIsOpened] = useState(false);
-
-  const toggleOpen = () => {
-    setIsOpened(!isOpened);
-  };
+  const opacity = isOpened ? 1 : 0;
 
 
   return (
-    <button onClick={toggleOpen} className='note'>
-        <div className="top_part">
-            <div className="header">
-              <h2>Note Name</h2>
-              <span>Added: 08 April 2024</span>
-            </div>
+    <motion.button layout onClick={ () => setIsOpened(!isOpened)} className='note' /*whileHover={{ scale: 1.1, transition: .2}}*/ transition={{layout: { duration: .1, type: 'spring', stiffness: 100, damping:15} }}>
+        <motion.div className="top_part">
+            <motion.div className="header">
+              <motion.h2 layout h2>Note Name</motion.h2>
+              <motion.span layout span>Added: 08 April 2024</motion.span>
+            </motion.div>
 
-            <div className='options'>
-              <button className={`edit note_btn ${isOpened? '' : 'hidden'}`}></button>
-              <button className='delete note_btn'></button>
-            </div>
-        </div>
-        <div className={`deviding ${isOpened? '' : 'hidden'}`}></div>
-        <div className={`subheader ${isOpened? '' : 'hidden'}`}>
-          <h3>Header 1</h3>
-          <span>Lorem ipsum dolor sit amet consectetur. Aliquam dolor massa est a interdum lacus non sed ipsum. Scelerisque tristique faucibus suspendisse leo</span>
-        </div>
-        <div className={`indicators ${isOpened? '' : 'hidden'}`}>
+            <motion.div className='options'>
+            <motion.button layout className= {`edit note_btn ${isOpened ? '' : 'hidden'}`}></motion.button>
+              <motion.button layout className= "delete note_btn"></motion.button>
+            </motion.div>
+        </motion.div>
+      <motion.div animate = {{opacity: opacity, transition: 1}} className={`deviding ${isOpened? '' : 'hidden'}`}></motion.div>
+        <motion.div className={`subheader ${isOpened? '' : 'hidden'}`}>
+          <motion.h3 animate = {{opacity: opacity, transition: 1}}>Header 1</motion.h3>
+          <motion.span animate = {{opacity: opacity, transition: 1}}>Lorem ipsum dolor sit amet consectetur. Aliquam dolor massa est a interdum lacus non sed ipsum. Scelerisque tristique faucibus suspendisse leo</motion.span>
+        </motion.div>
+        <motion.div className={`indicators ${isOpened? '' : 'hidden'}`}>
           <div className="indicator"></div>
           <div className="indicator"></div>
           <div className="indicator"></div>
-        </div>
-    </button>
+        </motion.div>
+    </motion.button>
   )
 }
 
--- src/components/Used_recource.jsx
+++ src/components/Used_recource.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styling/Used_recource.css';
 import right_arrow from '../assets/Icons/right_arrow.svg';
+import { motion } from 'framer-motion';
 
 const Used_recource = ({recource}) => {
   return (
-    <div >
+    <motion.div className="recource_container" whileHover={{ scale: 1.1, originX: 0}}>
         <a href={recource.link} className='used_recource' target='blank'>
             <div className="left_part">
                 <img src= {recource.icon} alt="" className='recource_icon'/>
@@ -13,7 +14,7 @@ const Used_recource = ({recource}) => {
 
             <img className='right_arrow' src = {right_arrow} alt="" />
         </a>
-    </div>
+    </motion.div>
   )
 }
 
